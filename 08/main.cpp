0d23548d9d2b0bf578faee98718dc1fcc707b1af
Artem Eroshev <43152363+UserKSIT@users.noreply.github.com>
2019-05-12 21:22:42 +0300

    Refactoring code
    Upgrade to class multithread sort

--- 08/main.cpp
+++ 08/main.cpp
@@ -5,159 +5,214 @@
 //  Created by Артём on 01/05/2019.
 //  Copyright © 2019 Ерошев Артём. All rights reserved.
 //
+
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <thread>
-#include <algorithm>
-
-
-constexpr size_t Mb = 1024*1024;
-constexpr size_t read_value = (2 * Mb - sizeof(std::thread) * 2);
-constexpr size_t MAXSIZE = read_value / sizeof(uint64_t);
 
 using typeNumber = uint64_t;
 
-size_t counter = 0;
-std::vector<std::string> file_list;
-
-void sort_(std::vector<typeNumber>::iterator begin_, std::vector<typeNumber>::iterator end_)
+class AsyncSort final
 {
-    std::sort(std::forward<std::vector<typeNumber>::iterator>(begin_), std::forward<std::vector<typeNumber>::iterator>(end_));
-}
-
-
-void quick_sort(std::vector<typeNumber>& buffer_)
-{
-    auto mid = buffer_.begin();
-    size_t shift = buffer_.size() / 2;
-    std::advance(mid, shift);
+private:
+    //We allocate 4Mb buffer, because quick sort may require O(n) memory
+    static constexpr size_t MAXSIZE = (4*1024*1024) / sizeof(typeNumber);
     
-    std::thread worker_1(sort_, buffer_.begin(), mid);
-    std::thread worker_2(sort_, mid, buffer_.end());
+    size_t counter;
     
-    worker_1.join();
-    worker_2.join();
+    std::vector<std::string> file_list;
+    std::vector<typeNumber> buffer_;
     
-    return;
-}
-
-bool writeFile(std::vector<typeNumber>& buffer_)
-{
-    std::string fileName = "semi_result" + std::to_string(counter) + ".bin";
-    std::ofstream file(fileName, std::ios::binary);
-    
-    if (!file)
+    void quick_sort()
     {
-        std::cerr << "Can't open " << fileName << '\n';
-        return false;
+        auto mid = buffer_.begin();
+        size_t shift = buffer_.size() / 2;
+        std::advance(mid, shift);
+        
+        std::thread worker_1(&AsyncSort::sort_, buffer_.begin(), mid);
+        std::thread worker_2(&AsyncSort::sort_, mid, buffer_.end());
+        
+        std::inplace_merge(buffer_.begin(), mid, buffer_.end());
+        
+        worker_1.join();
+        worker_2.join();
+        
+        return;
     }
-    counter++;
     
-    file.write(reinterpret_cast<char *> (buffer_.data()), read_value);
-    
-    file_list.push_back(fileName);
-    
-    return true;
-}
-
-bool fileMerge()
-{
-    std::ifstream file_1;
-    std::ifstream file_2;
-    std::ofstream result_file;
-    
-    std::string name_1;
-    std::string name_2;
-    std::string result_name;
-    
-    while (file_list.size() >= 2)
+    bool writeFile()
     {
-        name_1 = file_list.back();
-        file_list.pop_back();
-        name_2 = file_list.back();
-        file_list.pop_back();
+        std::string fileName = "semi_result" + std::to_string(counter) + ".bin";
+        std::ofstream file(fileName, std::ios::binary);
         
-        file_1.open(name_1, std::ios::binary);
-        file_2.open(name_2, std::ios::binary);
-        
-        result_name = "s_result" + std::to_string(counter) + ".bin";
-        counter--;
-        
-        result_file.open(result_name, std::ios::binary);
-        if (!result_file)
+        if (!file)
         {
-            std::cerr << "Can't open " << result_name << '\n';
+            std::cerr << "Can't open " << fileName << '\n';
+            return false;
+        }
+        counter++;
+        
+        file.write(reinterpret_cast<char *> (buffer_.data()), buffer_.size() * sizeof(typeNumber));
+        
+        file_list.push_back(fileName);
+        
+        return true;
+    }
+    
+    bool fileMerge()
+    {
+        std::ifstream file_1;
+        std::ifstream file_2;
+        std::ofstream result_file;
+        
+        std::string name_1;
+        std::string name_2;
+        std::string result_name;
+        
+        while (file_list.size() >= 2)
+        {
+            name_1 = file_list.back();
+            file_list.pop_back();
+            name_2 = file_list.back();
+            file_list.pop_back();
             
-            std::string name;
-            while(!file_list.empty())
+            file_1.open(name_1, std::ios::binary);
+            file_2.open(name_2, std::ios::binary);
+            
+            result_name = "s_result" + std::to_string(counter) + ".bin";
+            counter--;
+            
+            result_file.open(result_name, std::ios::binary);
+            if (!result_file)
             {
-                name = file_list.back();
-                file_list.pop_back();
-                std::remove(name.c_str());
+                std::cerr << "Can't open " << result_name << '\n';
+                
+                std::string name;
+                while(!file_list.empty())
+                {
+                    name = file_list.back();
+                    file_list.pop_back();
+                    std::remove(name.c_str());
+                }
+                
+                return false;
             }
             
-            return false;
-        }
-        
-        typeNumber number_1 = 0, number_2 = 0;
-        if (!file_1.eof() && !file_2.eof())
-        {
-            file_1.read(reinterpret_cast<char *>(&number_1), sizeof(typeNumber));
-            file_2.read(reinterpret_cast<char *>(&number_2), sizeof(typeNumber));
-        }
-        else
-            return false;
-        
-        while(!file_1.eof() && !file_2.eof())
-        {
-            if(number_1 < number_2)
+            typeNumber number_1 = 0, number_2 = 0;
+            if (!file_1.eof() && !file_2.eof())
             {
-                result_file.write(reinterpret_cast<char *>(&number_1), sizeof(typeNumber));
                 file_1.read(reinterpret_cast<char *>(&number_1), sizeof(typeNumber));
-            }
-            else
-            {
-                result_file.write(reinterpret_cast<char *>(&number_2), sizeof(typeNumber));
                 file_2.read(reinterpret_cast<char *>(&number_2), sizeof(typeNumber));
             }
-        }
-        
-        
-        while(!file_1.eof())
-        {
-            file_1.read(reinterpret_cast<char *>(&number_1), sizeof(typeNumber)/sizeof(char));
+            else
+                return false;
             
-            result_file.write(reinterpret_cast<char *>(&number_1), sizeof(typeNumber)/sizeof(char));
-        }
-
-        while(!file_2.eof())
-        {
-            file_2.read(reinterpret_cast<char *>(&number_2), sizeof(typeNumber)/sizeof(char));
+            while(!file_1.eof() && !file_2.eof())
+            {
+                if(number_1 < number_2)
+                {
+                    result_file.write(reinterpret_cast<char *>(&number_1), sizeof(typeNumber));
+                    file_1.read(reinterpret_cast<char *>(&number_1), sizeof(typeNumber));
+                }
+                else
+                {
+                    result_file.write(reinterpret_cast<char *>(&number_2), sizeof(typeNumber));
+                    file_2.read(reinterpret_cast<char *>(&number_2), sizeof(typeNumber));
+                }
+            }
             
-            result_file.write(reinterpret_cast<char *>(&number_2), sizeof(typeNumber)/sizeof(char));
+            
+            while(!file_1.eof())
+            {
+                file_1.read(reinterpret_cast<char *>(&number_1), sizeof(typeNumber)/sizeof(char));
+                
+                result_file.write(reinterpret_cast<char *>(&number_1), sizeof(typeNumber)/sizeof(char));
+            }
+            
+            while(!file_2.eof())
+            {
+                file_2.read(reinterpret_cast<char *>(&number_2), sizeof(typeNumber)/sizeof(char));
+                
+                result_file.write(reinterpret_cast<char *>(&number_2), sizeof(typeNumber)/sizeof(char));
+            }
+            
+            file_list.push_back(result_name);
+            
+            file_1.close();
+            file_2.close();
+            result_file.close();
+            
+            std::remove(name_1.c_str());
+            std::remove(name_2.c_str());
         }
         
-        file_list.push_back(result_name);
-        
-        file_1.close();
-        file_2.close();
-        result_file.close();
-        
-        std::remove(name_1.c_str());
-        std::remove(name_2.c_str());
+        return true;
     }
     
-    return true;
-}
+public:
+    AsyncSort()
+    :counter(0),
+    buffer_(MAXSIZE)
+    {}
+    
+    AsyncSort(const AsyncSort &) = delete;
+    AsyncSort(AsyncSort &&) = delete;
+    
 
+    void process(std::ifstream & file)
+    {
+        while (!file.eof())
+        {
+            for (size_t i = 0; i != MAXSIZE; i++)
+            {
+                if (file.read(reinterpret_cast<char *>(&buffer_[i]), sizeof(uint64_t)).eof())
+                {
+                    while(i != MAXSIZE)
+                    {
+                        buffer_[i++] = 0;
+                    }
+                    break;
+                }
+            }
+            quick_sort();
+            writeFile();
+        }
+        fileMerge();
+    }
+    
+    static void sort_(std::vector<typeNumber>::iterator left, std::vector<typeNumber>::iterator right)
+    {
+        if (right - left <= 1)
+            return;
+    
+        typeNumber mid = *(left + (right - left) / 2);
+        std::vector<typeNumber>::iterator yetLeft = left, yetRight = right - 1;
+    
+        while (yetLeft <= yetRight)
+        {
+            while (*yetLeft < mid) yetLeft++;
+            while (*yetRight > mid) yetRight--;
+                
+            if (yetLeft <= yetRight)
+            {
+                std::swap(*yetLeft, *yetRight);
+                yetLeft++;
+                yetRight--;
+            }
+        }
+    
+        if (left < yetRight)
+            sort_(left, yetRight + 1);
+        if (yetLeft < right)
+            sort_(yetLeft, right);
+    }
+};
 
 
 int main()
 {
-    std::vector<typeNumber> buffer_(MAXSIZE);
-    
-    const char* fileName = "data.bin";
+    std::string fileName = "data.bin";
     std::ifstream file(fileName, std::ios::binary);
     if (!file)
     {
@@ -165,18 +220,8 @@ int main()
         return 1;
     }
     
-    while (!file.eof())
-    {
-        file.read(reinterpret_cast<char *> (buffer_.data()), read_value);
-        
-        if(file.good())
-        {
-            quick_sort(buffer_);
-            writeFile(buffer_);
-        }
-    }
-    
-    fileMerge();
+    AsyncSort machine;
+    machine.process(file);
     
     return 0;
 }
